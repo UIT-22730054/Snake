650a7a257687f65dfe943b8b01c9caef7f6aecce
Nguyễn Minh Hải <127112460+UIT-22730030@users.noreply.github.com>
2023-03-18 19:43:57 +0700

    Update main.cpp
    xu_ly_ran

--- main.cpp
+++ main.cpp
@@ -179,4 +179,23 @@ void ve_ran(int toadox[], int toadoy[])
 		}
 	}
 }
+void xu_ly_ran(int toadox[], int toadoy[], int x, int y, int& xqua, int& yqua)
+{
+	//b1: them toa do moi vao dau mang
+	them(toadox, x);
+	them(toadoy, y);
+	if (kt_ran_an_qua(xqua, yqua, toadox[0], toadoy[0]) == false)
+	{
+		//b2: xoa toa do cuoi mang
+		xoa(toadox, sl - 1);
+		xoa(toadoy, sl - 1);
+
+	}
+	else
+	{
+		tao_qua(xqua, yqua, toadox, toadoy);
+	}
+	//b3: ve ran
+	ve_ran(toadox, toadoy);
+}
 
